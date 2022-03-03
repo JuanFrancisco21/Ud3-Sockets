7a35d21e4c17fe1a14fc42f3184bfb9ea0cdb417
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2022-03-03 15:56:15 +0100

    Proyecto funcional

--- src/main/java/com/iesfranciscodelosrios/UD3Socket/controller/HiloCliente.java
+++ src/main/java/com/iesfranciscodelosrios/UD3Socket/controller/HiloCliente.java
@@ -30,11 +30,12 @@ public class HiloCliente implements Runnable {
 
 				mensaje = (Mensaje) in.readObject();
 				
-				cliente(!mensaje.getUser().getAdministrador());
-				
-				login(mensaje.getDescripcion());
-				
-				
+				if (mensaje.getComando()==100) {
+					cliente(!mensaje.getUser().getAdministrador());					
+				}else {
+					login(mensaje.getDescripcion());					
+				}
+
 
 			} catch (IOException e) {
 				if ("Connection reset".equals(e.getMessage())) {
@@ -44,14 +45,14 @@ public class HiloCliente implements Runnable {
 				if (!timeToStop) {
 					e.printStackTrace();
 				}
-			} catch (ClassNotFoundException e) {	
+			} catch (ClassNotFoundException e) {
 			} catch (Exception e) {
 				System.out.println("Error hilo cliente");
 			}
 		}
 
 	}
-	
+
 	private void cliente(boolean b) {
 		Platform.runLater(() -> {
 			try {
@@ -59,54 +60,63 @@ public class HiloCliente implements Runnable {
 				if (!mensaje.getUser().getAdministrador()) {
 					switch (mensaje.getComando()) {
 					case 100:
-						if (mensaje.getCuenta()!=null) {
-							ControllerClientes.cuenta = mensaje.getCuenta();	
-							ControllerClientes.mensaje = mensaje;
-							ControllerClientes.cofigureOnServerResponse();
-						}
-						break;
-						
-					default:break;
-					}
-				} else {
-					switch (mensaje.getComando()) {
-					case 100:
-						if (mensaje.getCuenta()!=null) {
+						if (mensaje.getCuenta() != null) {
 							ControllerClientes.cuenta = mensaje.getCuenta();
 							ControllerClientes.mensaje = mensaje;
 							ControllerClientes.cofigureOnServerResponse();
 						}
 						break;
-						
-					default:break;
+
+					default:
+						break;
 					}
+				} else {
+					
+					if (mensaje.getDescripcion().equals("Error al crear a un usuario")
+							|| mensaje.getDescripcion().equals("Error al borrar cuenta a un usuario")
+							|| mensaje.getDescripcion().equals("Cliente con cuenta asignada")) {
+						ControllerClientes.showError(mensaje.getDescripcion());
+					} else {
+						
+							if (mensaje != null) {
+								if (mensaje.getDescripcion().equals("Nueva cuenta asignada al usuario")) {
+									ControllerClientes.showInfo(mensaje.getDescripcion());
+								}
+								ControllerClientes.mensaje = mensaje;
+								ControllerClientes.cofigureOnServerResponse();
+							}
+						
+						
+					}
+
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		});
-		
+
 	}
 
-	//CONEXION CON EL LOGIN
+	// CONEXION CON EL LOGIN
 	public void login(String mesagge) {
-		
+
 		switch (mesagge) {
 		case "Usuario no registrado":
 		case "Usuario ya conectado":
 		case "Error al logear.":
 			String m = mensaje.getDescripcion();
-			MainController.showWarning(m);
+			LoginController.showWarning(m);
 			System.out.println(m);
 			break;
 		// EN CASO DE INICIAR SESION BIEN MUESTRA LA PANTALLA CON LOS DATOS.
 		case "logeado":
-			MainController.mensaje = mensaje;
-			MainController.usuario = mensaje.getUser();
-			MainController.showView();
+			LoginController.mensaje = mensaje;
+			LoginController.usuario = mensaje.getUser();
+			LoginController.showView();
 			break;
-			
-		default:break;
+
+		default:
+			break;
 		}
 	}
 
